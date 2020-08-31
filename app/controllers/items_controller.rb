8e5216de59d5c6b577d56a3ffed3c12a432df123
Mocadi-dot <59927623+mocadi-dot@users.noreply.github.com>
2020-08-31 18:37:16 +0200

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,25 @@
-class ItemsController < ApplicationController
-   def show
-      @item = finderItem
-   end
+class ItemsController < ActionController::Base
+
+    def index
+        @items = Item.all
+    end
+
+#  def show
+#        @item = Item.find(params[:id])
+#   end
+
+#    def create
+ #       @item = Item.create
+ #   end
+
+
+ #   def destroy
+ #       @item = Item.find(params[:id])
+  #      @item.destroy
+#    end
+
+ #   def update
+  #      @item = Item.find(params[:id])
+ #       @item.update
+  #  end
 end
