63eba1ef98796e638a5bf94b24983f65fd141a95
rosvali <rosval6578@gmail.com>
2020-08-31 14:39:02 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # i = Item.create(title: "lollol", description: "llllllllllllllll", price: 2, image_url: "lol.png")
+
+Item.destroy_all
+
+20.times do 
+	Item.create(title: "Chat mignon", description: "Ce chat est trop mignoooooon", price: 10, image_url: "https://www.wanimo.com/veterinaire/wp-content/uploads/2019/12/prenoms-chaton-r-2020@2x.jpg");
+end
+
+puts "20 items created"
