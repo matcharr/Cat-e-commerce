d065cf95b0ba062059d0a5d66a02c2f2bcf05760
rosvali <rosval6578@gmail.com>
2020-08-31 14:01:14 +0200

    validation item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+	validates :title, :price, :description, :image_url, presence: true
+
+	validates :title, length: { in: 6..20 }
+	validates :description, length: { minimum: 10 }
+	validates :price, numericality: { greater_than: 0, less_than: 100 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# i = Item.create(title: "lollol", description: "llllllllllllllll", price: 2, image_url: "lol.png")
