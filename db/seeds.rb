759df4f3f4bf34fbc2e312a9f73d3ca8f05f65d3
rosvali <rosval6578@gmail.com>
2020-09-02 19:02:05 +0200

    new nice seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,28 @@
 Item.destroy_all
 Order.destroy_all
 Cart.destroy_all
-ItemOrderTable.destroy_all
+OrderItemTable.destroy_all
 
-20.times do 
-	Item.create(title: "Chat mignon", description: "Ce chat est trop mignoooooon", price: 10, image_url: "https://www.wanimo.com/veterinaire/wp-content/uploads/2019/12/prenoms-chaton-r-2020@2x.jpg");
-end
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/1870376/pexels-photo-1870376.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/2061057/pexels-photo-2061057.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/96938/pexels-photo-96938.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/127027/pexels-photo-127027.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/735423/pexels-photo-735423.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/248280/pexels-photo-248280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/1981111/pexels-photo-1981111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/1084425/pexels-photo-1084425.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/1543793/pexels-photo-1543793.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/290164/pexels-photo-290164.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/1404825/pexels-photo-1404825.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/1302290/pexels-photo-1302290.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/37337/cat-silhouette-cats-silhouette-cat-s-eyes.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/1056467/pexels-photo-1056467.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/2581153/pexels-photo-2581153.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/257532/pexels-photo-257532.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/1852914/pexels-photo-1852914.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/209037/pexels-photo-209037.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+Item.create(title: Faker::Creature::Cat.name, description: Faker::Movie.quote, price: rand(10..50), image_url: "https://images.pexels.com/photos/2194261/pexels-photo-2194261.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
 
 puts "20 items created"
