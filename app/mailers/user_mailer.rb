624ee9a51130d9acaa608caef1a0fc9a534502e0
rosvali <rosval6578@gmail.com>
2020-09-03 15:00:35 +0200

    adding order_send

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,26 @@
 class UserMailer < ApplicationMailer
 
 default from: 'no-reply@CatWeb.fr'
- 
+
   def welcome_email(user)
-    @user = user 
+    @user = user
     @url  = 'http://localhost:3000/' 
 
 		mail(
 			to: @user.email,
 			delivery_method_options: { api_key: ENV['MAIL_API_KEY'], secret_key: ENV['MAIL_API_KEY_SECRET'] }
 		)
+  end
+
+  def order_email(user)
+  	@user = user
+  	@url = 'http://localhost:3000/'
+  	@items = @user.orders.last.items
+
+  	mail(
+  		to: @user.email,
+			delivery_method_options: { api_key: ENV['MAIL_API_KEY'], secret_key: ENV['MAIL_API_KEY_SECRET'] }
+  	)
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
+	after_create :order_send
+
+	def order_send
+    UserMailer.order_email(self.user).deliver_now
+	end
+
 	belongs_to :user
 	has_many :order_item_tables
 	has_many :items, through: :order_item_tables
