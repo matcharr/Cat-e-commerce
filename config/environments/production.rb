805b9d4d37680ab4438f60f51e02d1639b82c348
rosvali <rosval6578@gmail.com>
2020-09-03 21:46:33 +0200

    lol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 # config.action_mailer.default_url_options = { :host => 'YOURAPPNAME.herokuapp.com' }
-  config.action_mailer.delivery_method = :mailjet_api
-  
+  config.action_mailer.delivery_method = :mailjet_api  
 end
 
 
